3db555b5591248f739efd68187a291542efb6be9
trickybestia <trickybestia@gmail.com>
2025-03-07 17:23:14 +0300

    Add better collision check

--- src/frame_renderer.sv
+++ src/frame_renderer.sv
@@ -23,7 +23,9 @@ typedef enum {
     MOVE_PIPES,
     DRAW_BACKGROUND_START,
     DRAW_BACKGROUND,
-    DRAW_BIRD,
+    DRAW_BIRD_START,
+    DRAW_BIRD_LOOP,
+    DRAW_BIRD_END,
     DRAW_PIPES_UPDATE_INV_X,
     DRAW_PIPES_WORK,
     DRAW_PIPES_CHECK_COLLISION,
@@ -72,8 +74,9 @@ reg [HOR_ACTIVE_PIXELS_WIDTH-1:0] pipe_offset;
 reg [VER_ACTIVE_PIXELS_WIDTH*PIPES_COUNT-1:0] pipes_y;
 
 // DRAW_BIRD
-reg  [$clog2(BIRD_WIDTH)-1:0]  draw_bird_x;
-reg  [$clog2(BIRD_HEIGHT)-1:0] draw_bird_y;
+reg  [$clog2(BIRD_WIDTH)-1:0]  draw_bird_x, draw_bird_last_x;
+reg  [$clog2(BIRD_HEIGHT)-1:0] draw_bird_y, draw_bird_last_y;
+reg  [7:0]                     bird_image_rom_addr;
 wire                           bird_image_rom_out;
 
 // DRAW_PIPES
@@ -83,36 +86,40 @@ reg  [VER_ACTIVE_PIXELS_WIDTH-1:0] draw_pipes_y;
 wire [VER_ACTIVE_PIXELS_WIDTH-1:0] draw_pipes_pipe_y = pipes_y[draw_pipes_pipe * VER_ACTIVE_PIXELS_WIDTH+:VER_ACTIVE_PIXELS_WIDTH];
 reg  [HOR_ACTIVE_PIXELS_WIDTH:0]   draw_pipes_inv_x;
 reg  [HOR_ACTIVE_PIXELS_WIDTH-1:0] draw_pipes_real_x;
+reg                                draw_pipes_check_collision;
 
 // Assignments
 
 // Modules
 
 bird_image_rom bird_image_rom_inst (
-    .dout(bird_image_rom_out),                                  //output [0:0] dout
-    // Divide because source image is 17x12 and we need to upscale it to 34x24
-    .ad((draw_bird_y / 2) * (BIRD_WIDTH / 2) + draw_bird_x / 2) //input [7:0] ad
+    .dout(bird_image_rom_out), //output [0:0] dout
+    .ad(bird_image_rom_addr)   //input [7:0] ad
 );
 
 // Processes
 
 always_ff @(posedge clk) begin
     if (rst) begin
-        state             <= CHECK_LOSE;
-        pipe_offset       <= '0;
-        pipes_y           <= '0;
-        draw_bird_x       <= '0;
-        draw_bird_y       <= '0;
-        draw_pipes_pipe   <= '0;
-        draw_pipes_x      <= '0;
-        draw_pipes_y      <= '0;
-        draw_pipes_inv_x  <= '0;
-        draw_pipes_real_x <= '0;
-        wr_en             <= '0;
-        wr_addr           <= '0;
-        wr_data           <= '0;
-        lose              <= 1'b0;
-        bird_y            <= VER_ACTIVE_PIXELS / 2 - BIRD_HEIGHT / 2;
+        state                      <= CHECK_LOSE;
+        pipe_offset                <= '0;
+        pipes_y                    <= '0;
+        draw_bird_x                <= '0;
+        draw_bird_y                <= '0;
+        draw_bird_last_x           <= '0;
+        draw_bird_last_y           <= '0;
+        bird_image_rom_addr        <= '0;
+        draw_pipes_pipe            <= '0;
+        draw_pipes_x               <= '0;
+        draw_pipes_y               <= '0;
+        draw_pipes_inv_x           <= '0;
+        draw_pipes_real_x          <= '0;
+        draw_pipes_check_collision <= '0;
+        wr_en                      <= '0;
+        wr_addr                    <= '0;
+        wr_data                    <= '0;
+        lose                       <= 1'b0;
+        bird_y                     <= VER_ACTIVE_PIXELS / 2 - BIRD_HEIGHT / 2;
     end else if (ce) case (state)
         CHECK_LOSE: begin
             state <= lose ? DRAW_BACKGROUND_START : MOVE_BIRD;
@@ -148,19 +155,33 @@ always_ff @(posedge clk) begin
             wr_addr <= wr_addr + 1;
 
             if (wr_addr == HOR_ACTIVE_PIXELS * VER_ACTIVE_PIXELS - 1) begin
-                state <= DRAW_BIRD;
+                state <= DRAW_BIRD_START;
             end
         end
-        DRAW_BIRD: begin
+        DRAW_BIRD_START: begin
+            wr_en <= '0;
+
+            draw_bird_x <= 1;
+            
+            bird_image_rom_addr <= '0;
+
+            state <= DRAW_BIRD_LOOP;
+        end
+        DRAW_BIRD_LOOP: begin
+            draw_bird_last_x <= draw_bird_x;
+            draw_bird_last_y <= draw_bird_y;
+
             wr_en   <= 1'b1;
-            wr_addr <= (bird_y + draw_bird_y) * HOR_ACTIVE_PIXELS + BIRD_HOR_OFFSET + draw_bird_x;
+            wr_addr <= (bird_y + draw_bird_last_y) * HOR_ACTIVE_PIXELS + BIRD_HOR_OFFSET + draw_bird_last_x;
             wr_data <= bird_image_rom_out;
+
+            bird_image_rom_addr <= (draw_bird_y / 2) * (BIRD_WIDTH / 2) + draw_bird_x / 2;
 
             if (draw_bird_x == BIRD_WIDTH - 1) begin
                 if (draw_bird_y == BIRD_HEIGHT - 1) begin
                     draw_bird_x <= '0;
                     draw_bird_y <= '0;
-                    state       <= DRAW_PIPES_UPDATE_INV_X;
+                    state       <= DRAW_BIRD_END;
                 end else begin
                     draw_bird_y <= draw_bird_y + 1;
                     draw_bird_x <= '0;
@@ -168,6 +189,12 @@ always_ff @(posedge clk) begin
             end else begin
                 draw_bird_x <= draw_bird_x + 1;
             end
+        end
+        DRAW_BIRD_END: begin
+            wr_addr <= (bird_y + draw_bird_last_y) * HOR_ACTIVE_PIXELS + BIRD_HOR_OFFSET + draw_bird_last_x;
+            wr_data <= bird_image_rom_out;
+
+            state <= DRAW_PIPES_UPDATE_INV_X;
         end
         DRAW_PIPES_UPDATE_INV_X: begin
             wr_en             <= '0;
@@ -181,14 +208,18 @@ always_ff @(posedge clk) begin
             wr_addr <= draw_pipes_y * HOR_ACTIVE_PIXELS + draw_pipes_real_x;
             wr_data <= 1'b1;
 
+            bird_image_rom_addr <= (draw_pipes_y - bird_y) / 2 * BIRD_WIDTH / 2 + (draw_pipes_real_x - BIRD_HOR_OFFSET) / 2;
+            draw_pipes_check_collision <= draw_pipes_y >= bird_y
+                      && draw_pipes_y <= bird_y + BIRD_HEIGHT
+                      && draw_pipes_real_x >= BIRD_HOR_OFFSET
+                      && draw_pipes_real_x <= BIRD_HOR_OFFSET + BIRD_WIDTH;
+
             state <= DRAW_PIPES_CHECK_COLLISION;
         end
         DRAW_PIPES_CHECK_COLLISION: begin
-            if (wr_en && draw_pipes_y >= bird_y && draw_pipes_y <= bird_y + BIRD_HEIGHT
-                      && draw_pipes_real_x >= BIRD_HOR_OFFSET
-                      && draw_pipes_real_x <= BIRD_HOR_OFFSET + BIRD_WIDTH) begin
-                        lose <= 1'b1;
-                      end
+            if (wr_en && draw_pipes_check_collision && bird_image_rom_out) begin
+                lose <= 1'b1;
+            end
 
             if (draw_pipes_x == PIPE_WIDTH - 1 && draw_pipes_y == VER_ACTIVE_PIXELS - 1 && draw_pipes_pipe == PIPES_COUNT - 1) begin
                 draw_pipes_x    <= '0;
