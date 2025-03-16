76303a5fd66876e850320d5af021caf8eed1b7bb
trickybestia <trickybestia@gmail.com>
2025-03-16 14:14:49 +0300

    Make swap reg

--- src/pixel_iterator.sv
+++ src/pixel_iterator.sv
@@ -54,6 +54,8 @@ reg [Y_INTERNAL_WIDTH-1:0] y_internal;
 reg [X_INTERNAL_WIDTH-1:0] next_x_internal;
 reg [Y_INTERNAL_WIDTH-1:0] next_y_internal;
 
+reg next_swap;
+
 // Assignments
 
 assign x = x_internal > HOR_BACK_PORCH_PIXELS ? x_internal - HOR_BACK_PORCH_PIXELS : '0;
@@ -70,14 +72,14 @@ assign de = (x_internal >= HOR_BACK_PORCH_PIXELS
 // Processes
 
 always_comb begin
-    swap = 1'b0;
+    next_swap = 1'b0;
 
     if (x_internal == HOR_TOTAL_PIXELS - 1) begin
         next_x_internal = '0;
 
         if (y_internal == VER_TOTAL_PIXELS - 1) begin
             next_y_internal = '0;
-            swap            = 1'b1;
+            next_swap       = 1'b1;
         end else begin
             next_y_internal = y_internal + 1;
         end
@@ -91,9 +93,11 @@ always_ff @(posedge clk_rgb) begin
     if (rst) begin
         x_internal <= '0;
         y_internal <= '0;
+        swap       <= '0;
     end else if (ce) begin
         x_internal <= next_x_internal;
         y_internal <= next_y_internal;
+        swap       <= next_swap;
     end
 end
 
