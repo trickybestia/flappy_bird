e1369a8b1d3a120e9632665d531335043bf49f4f
trickybestia <trickybestia@gmail.com>
2025-04-30 22:34:33 +0300

    Complete pipes_list testbench and fix pipes_list module

--- src/pipes_list.sv
+++ src/pipes_list.sv
@@ -27,40 +27,51 @@ input pipe_t insert_data;
 input         iter_start;
 output reg    iter_done;
 input  pipe_t iter_in;
-output        iter_out;
+output pipe_t iter_out;
 input         iter_remove;
 
 // Wires/regs
 
-wire [4:0] fifo_count;
+wire [4:0] fifo_count, items_count;
 
 reg       iter_done_next;
 reg [4:0] iter_index, iter_index_next;
 
 // Assignments
 
+assign items_count = iter_done ? fifo_count : fifo_count + 1;
+
 // Modules
 
-pipes_list_fifo pipes_list_fifo_inst (
-    .Data  (insert_en ? insert_data : iter_in),         // input [21:0] Data
-    .Clk   (clk),                                       // input Clk
-    .WrEn  (insert_en || (!iter_done && !iter_remove)), // input WrEn
-    .RdEn  (!iter_done_next),                           // input RdEn
-    .Reset (rst),                                       // input Reset
-    .Wnum  (fifo_count),                                // output [4:0] Wnum
-    .Q     (iter_out),                                  // output [21:0] Q
-    .Empty (),                                          // output Empty
-    .Full  ()                                           // output Full
+fifo #(
+    .SIZE(16),
+    .DATA_WIDTH($bits(pipe_t))
+) fifo_inst (
+    .clk,
+    .rst,
+    .ce,
+    .wr_en   (insert_en || (!iter_done && !iter_remove)),
+    .wr_data (insert_en ? insert_data : iter_in),
+    .rd_en   (!iter_done_next),
+    .rd_data (iter_out),
+    .empty   (),
+    .full    (),
+    .count   (fifo_count)
 );
 
 // Processes
+
+initial begin
+    iter_done  <= 1;
+    iter_index <= 0;
+end
 
 always_comb begin
     if (iter_start) begin
         iter_index_next = 0;
     end else if (!iter_done) begin
-        if (iter_index == fifo_count - 1) begin
-            iter_index_next = iter_index;
+        if (iter_index == items_count - 1) begin
+            iter_index_next = 0;
         end else if (!iter_remove) begin
             iter_index_next = iter_index + 1;
         end else begin
@@ -81,10 +92,12 @@ end
 
 
 always_comb begin
-    if (!iter_start && iter_index == fifo_count - 1) begin
-        iter_done_next = 1;
+    if (iter_start) begin
+        iter_done_next = fifo_count == 0;
+    end else if (!iter_done) begin
+        iter_done_next = iter_index == items_count - 1;
     end else begin
-        iter_done_next = 0;
+        iter_done_next = iter_done;
     end
 end
 
--- tb/pipes_list_tb.sv
+++ tb/pipes_list_tb.sv
@@ -1,5 +1,7 @@
 `timescale 1ns / 1ps
 
+`include "pipe_t.sv"
+
 module pipes_list_tb;
 
 // Parameters
@@ -9,17 +11,41 @@ module pipes_list_tb;
 // Wires/regs
 
 reg clk;
+reg rst;
+reg ce;
+
+reg    insert_en;
+pipe_t insert_data;
+
+reg    iter_start;
+wire   iter_done;
+pipe_t iter_in;
+pipe_t iter_out;
+reg    iter_remove;
 
 // Assignments
 
 // Modules
 
+pipes_list uut (
+    .clk,
+    .rst,
+    .ce,
+    .insert_en,
+    .insert_data,
+    .iter_start,
+    .iter_done,
+    .iter_in,
+    .iter_out,
+    .iter_remove
+);
+
 // Processes
 
 always begin
-    clk <= 0;
+    clk = 0;
     #5;
-    clk <= 1;
+    clk = 1;
     #5;
 end
 
@@ -27,8 +53,84 @@ initial begin
     $dumpfile("pipes_list_tb.vcd");
     $dumpvars;
     
+    rst         = 0;
+    ce          = 1;
+    insert_en   = 0;
+    insert_data = 0;
+    iter_start  = 0;
+    iter_in     = 0;
+    iter_remove = 0;
     
-        
+    #10;
+
+    insert_en = 1;
+
+    for (integer i = 1; i != 5; i++) begin
+        insert_data = i;
+
+        #10;
+    end
+
+    insert_en = 0;
+
+    repeat (5) begin
+        iter_start = 1;
+        #10 iter_start = 0;
+
+        while (!iter_done) begin
+            iter_in = iter_out;
+
+            #10;
+        end
+    end
+
+    repeat (4) begin
+        iter_start = 1;
+        #10 iter_start = 0;
+
+        while (!iter_done) begin
+            iter_in = iter_out + 1;
+
+            #10;
+        end
+    end
+
+    repeat (1) begin
+        iter_start = 1;
+        #10 iter_start = 0;
+
+        while (!iter_done) begin
+            iter_in = iter_out;
+
+            #10;
+        end
+    end
+
+    repeat (4) begin
+        iter_start = 1;
+        #10 iter_start = 0;
+
+        iter_remove = 1;
+        #10 iter_remove = 0;
+
+        while (!iter_done) begin
+            iter_in = iter_out;
+
+            #10;
+        end
+    end
+
+    repeat (1) begin
+        iter_start = 1;
+        #10 iter_start = 0;
+
+        while (!iter_done) begin
+            iter_in = iter_out;
+
+            #10;
+        end
+    end
+
     $finish;
 end
 
