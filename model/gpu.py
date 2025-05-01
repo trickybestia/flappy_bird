b261eef6e43bf90631be0cccde7664deb3af76ba
trickybestia <trickybestia@gmail.com>
2025-05-01 18:15:26 +0300

    Add boundary check in gpu model

--- model/gpu.py
+++ model/gpu.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 import pygame.draw
 from pygame import Surface
 
+from .parameters import *
+
 
 @dataclass
 class GpuOp:
@@ -28,6 +30,14 @@ class Gpu:
         if op.mem_en:
             for rel_x in range(op.width):
                 for rel_y in range(op.height):
+                    if (
+                        not 0 <= op.x + rel_x < HOR_ACTIVE_PIXELS
+                        or not 0 <= op.y + rel_y < VER_ACTIVE_PIXELS
+                    ):
+                        raise ValueError(
+                            "gpu primitive coordinates out of bounds"
+                        )
+
                     mem_address = (
                         op.mem_addr
                         + (rel_y >> op.scale) * (op.width >> op.scale)
@@ -39,6 +49,14 @@ class Gpu:
 
                     self.screen.set_at((op.x + rel_x, op.y + rel_y), color)
         else:
+            if (
+                not 0 <= op.x < HOR_ACTIVE_PIXELS
+                or not 0 <= op.y < VER_ACTIVE_PIXELS
+                or not 0 <= op.x + op.width <= HOR_ACTIVE_PIXELS
+                or not 0 <= op.y + op.height <= VER_ACTIVE_PIXELS
+            ):
+                raise ValueError("gpu primitive coordinates out of bounds")
+
             pygame.draw.rect(
                 self.screen,
                 (255, 255, 255) if op.color else (0, 0, 0),
